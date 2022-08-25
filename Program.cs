89249d40588d7eca795a758698e2f3cf97d5293c
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-25 20:02:04 +0530

    dequeue from the bigining

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace StacksAndQueue
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            DequeueElementsQueue dequeue = new DequeueElementsQueue();
+            dequeue.Enqueue(56);
+            dequeue.Enqueue(30);
+            dequeue.Enqueue(70);
+            dequeue.Display();
+            while (!dequeue.IsEmpty())
+            {
+                dequeue.Dequeue();
+            }
+        }
+    }
+}
