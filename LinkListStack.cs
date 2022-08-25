ca11cc491f92bac20d0efab87f8817cf49cb3ec4
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-25 19:36:27 +0530

    created a stack

---
+++ LinkListStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksAndQueue
+{
+    public class LinkListStack
+    {
+        Node head;
+        Node current;
+        public LinkListStack()
+        {
+            this.head = null;
+        }
+        public void Push(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} data is inserted \n", node.data);
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if(temp == null)
+            {
+                Console.WriteLine("LinkedList is empty\n");
+                return;
+            }
+            while(temp != null)
+            {
+                Console.Write(temp.data + " \n");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StacksAndQueue
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            LinkListStack stack = new LinkListStack();
+
+            stack.Push(56);
+            stack.Push(30);
+            stack.Push(70);
+            stack.Display();
+        }
+    }
+}
