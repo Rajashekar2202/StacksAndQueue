e1a2b60ec27f832880bc5014331a2c0fdfaaf1c0
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-25 19:45:52 +0530

    peep and pop from stack

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StacksAndQueue
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+         
+            PeakAndPopStack peakAndPopStack = new PeakAndPopStack();
+            peakAndPopStack.Push(56);
+            peakAndPopStack.Push(30);
+            peakAndPopStack.Push(70);
+            peakAndPopStack.Display();
+            while (!peakAndPopStack.IsEmpty())
+            {
+                peakAndPopStack.Peek();
+                peakAndPopStack.Pop();
+            }
+        }
+    }
+}
