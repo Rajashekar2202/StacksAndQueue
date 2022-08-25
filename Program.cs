485033fffb324eba490a37d2398e707d95ab3b5e
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-25 19:54:35 +0530

    created a queue

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace StacksAndQueue
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            QueueLinklist queue = new QueueLinklist();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
+        }
+    }
+}
